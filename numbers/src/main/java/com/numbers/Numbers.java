f53725b395ffd03a9e466447df5a31e063a29f88
Amin Khorsandiaghai <aminkhorsandi@gmail.com>
2015-03-25 23:50:17 +0100

    Some more tests.

--- numbers/src/main/java/com/numbers/Numbers.java
+++ numbers/src/main/java/com/numbers/Numbers.java
@@ -1,12 +1,28 @@
 package com.numbers;
 
+import java.util.Scanner;
+
 public class Numbers {
-    public static int getValue(final int num) throws IllegalArgumentException {
-        if (num > 0) {
-            return num;
+    protected static int getValue(final int num) 
+        throws IllegalArgumentException {
+        if (num > 0 && num < 10) {
+            switch (num) {
+            case 1: return 1;
+            case 2: return 1;
+            default: return (getValue(num-1) + getValue(num-2));
+            }
         } else {
             throw new IllegalArgumentException();
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Please enter an integer (1-9)"); 
+        Scanner scan = new Scanner(System.in);
+        //        String s = scan.next();
+        int i = scan.nextInt();
+        int result = getValue(i);
+        System.out.println("The result is:" + Integer.toString(result)); 
     }
 
 }
--- numbers/src/test/java/com/numbers/NumbersTest.java
+++ numbers/src/test/java/com/numbers/NumbersTest.java
@@ -5,8 +5,8 @@ import static org.junit.Assert.*;
 
 
 /**
- * Set of unit tests to test the Number 
- * function.
+ * Set of unit tests to test the Number's 
+ * getValue function.
  * @author Amin
  */
 
@@ -18,7 +18,6 @@ public class NumbersTest {
         num = new Numbers();
     }
 
-
     @Test(expected = IllegalArgumentException.class)
     public final void throughExceptionIfInputIsNotPositiveInteger() {
         num.getValue(-1);
@@ -29,6 +28,36 @@ public class NumbersTest {
         num.getValue(0);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public final void throughExceptionIfInputBiggerThanNine() {
+        num.getValue(10);
+    }
+
+    @Test
+    public final void outPutOneIfInputOne() {
+        assertEquals(1, num.getValue(1));
+    }
+
+    @Test
+    public final void outPutOneIfInputTwo() {
+        assertEquals(1, num.getValue(2));
+    }
+
+    @Test
+    public final void outPutCorrect() {
+        assertEquals(num.getValue(1) + num.getValue(2), num.getValue(3));
+        assertEquals(num.getValue(7) + num.getValue(8), num.getValue(9));
+        assertNotEquals(num.getValue(3) + num.getValue(4), num.getValue(6));
+    }
+
+    @Test
+    public final void outPutCorrectOnSomeRandomInput() {
+        assertEquals(21, num.getValue(8));
+        assertEquals(8, num.getValue(6));
+        assertEquals(2, num.getValue(3));
+        assertEquals(34, num.getValue(9));
+    }
 }
+
 
 
