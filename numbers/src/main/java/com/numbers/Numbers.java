95acfc8c84fb8ac883e58f8c0644335d95fbea52
Amin Khorsandiaghai <aminkhorsandi@gmail.com>
2015-04-14 21:36:19 +0200

    More algorithms and removed bad tests!

--- numbers/src/main/java/com/numbers/Numbers.java
+++ numbers/src/main/java/com/numbers/Numbers.java
@@ -3,26 +3,41 @@ package com.numbers;
 import java.util.Scanner;
 
 public class Numbers {
-    protected static int getValue(final int num) 
-        throws IllegalArgumentException {
-        if (num > 0 && num < 10) {
-            switch (num) {
-            case 1: return 1;
-            case 2: return 1;
-            default: return (getValue(num-1) + getValue(num-2));
-            }
-        } else {
-            throw new IllegalArgumentException();
-        }
+    protected static long getValue(final int num) {
+        //return tailRecursiveFib(num, 1, 0);
+        //return recFibonacci(num);
+        return fibonacci(num);
     }
 
     public static void main(String[] args) {
-        System.out.println("Please enter an integer (1-9)"); 
+        System.out.println("Please enter an integer "); 
         Scanner scan = new Scanner(System.in);
-        //        String s = scan.next();
         int i = scan.nextInt();
-        int result = getValue(i);
-        System.out.println("The result is:" + Integer.toString(result)); 
+        long result = getValue(i);
+        System.out.println("The result is:" + Long.toString(result)); 
     }
+
+    private static long recFibonacci(final int number) {
+        switch (number) {
+        case 1 : return 1;
+        case 2 : return 1;
+        default : return recFibonacci(number -1) + recFibonacci(number - 2);
+        }
+    }
+
+    private static long fibonacci(final int number) {
+        long x=1, y=1;
+        for (int i = 1; i < number; i++) {
+            long z = x + y; 
+            y = x;
+            x = z;
+        }
+        return y;
+    }
+
+    public static long tailRecursiveFib(int n, long a, long b) {
+        return n == 0 ? b : tailRecursiveFib(n-1, a+b, a);
+    }
+
 
 }
--- numbers/src/test/java/com/numbers/NumbersTest.java
+++ numbers/src/test/java/com/numbers/NumbersTest.java
@@ -18,44 +18,22 @@ public class NumbersTest {
         num = new Numbers();
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public final void throughExceptionIfInputIsNotPositiveInteger() {
-        num.getValue(-1);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public final void throughExceptionIfInputIsZero() {
-        num.getValue(0);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public final void throughExceptionIfInputBiggerThanNine() {
-        num.getValue(10);
-    }
-
     @Test
     public final void outPutOneIfInputOne() {
-        assertEquals(1, num.getValue(1));
+        assertEquals(1L, num.getValue(1));
     }
 
     @Test
     public final void outPutOneIfInputTwo() {
-        assertEquals(1, num.getValue(2));
-    }
-
-    @Test
-    public final void outPutCorrect() {
-        assertEquals(num.getValue(1) + num.getValue(2), num.getValue(3));
-        assertEquals(num.getValue(7) + num.getValue(8), num.getValue(9));
-        assertNotEquals(num.getValue(3) + num.getValue(4), num.getValue(6));
+        assertEquals(1L, num.getValue(2));
     }
 
     @Test
     public final void outPutCorrectOnSomeRandomInput() {
-        assertEquals(21, num.getValue(8));
-        assertEquals(8, num.getValue(6));
-        assertEquals(2, num.getValue(3));
-        assertEquals(34, num.getValue(9));
+        assertEquals(21L, num.getValue(8));
+        assertEquals(8L, num.getValue(6));
+        assertEquals(2L, num.getValue(3));
+        assertEquals(34L, num.getValue(9));
     }
 }
 
